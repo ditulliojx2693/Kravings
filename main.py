4a5dadabb653abc54e0b2c4e4c90131e5dd322b6
Hannah <hannahsalameh18@gmail.com>
2019-07-29 12:04:16 -0700

    fixing broken things

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ class HomePage(webapp2.RequestHandler):
 
 class QuizPage(webapp2.RequestHandler):
     def get(self):  # for a get request
-        quiz_template = the_jinja_env.get_template('quiz.html')
+        quiz_template = the_jinja_env.get_template('templates/quiz.html')
         self.response.write(quiz_template.render())  # the response
         self.response.write("quiz working")
 
 class ResultsPage(webapp2.RequestHandler):
     def get(self):  # for a get request
-        results_template = the_jinja_env.get_template('results.html')
+        results_template = the_jinja_env.get_template('templates/results.html')
         self.response.write(results_template.render())  # the response
         self.response.write("results working")
 
 class AboutUsPage(webapp2.RequestHandler):
     def get(self):  # for a get request
-        aboutUs_template = the_jinja_env.get_template('AboutUs.html')
+        aboutUs_template = the_jinja_env.get_template('templates/AboutUs.html')
         self.response.write(aboutUs_template.render())  # the response
         self.response.write("about us working")
 
