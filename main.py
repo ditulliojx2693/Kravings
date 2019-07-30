0d2c4779139f69105d37828c6ccb68b5bfa42b3f
cssi <cssi@cssis-MacBook-Pro-78.local>
2019-07-30 12:30:27 -0700

    done working on main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import jinja2
 import os
 from google.appengine.api import urlfetch
 
+
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
