e8eab447201bd4c950f3fb4997beee5d83c882d6
Hannah <hannahsalameh18@gmail.com>
2019-07-30 11:41:53 -0700

    should start working fully?

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,5 @@
 
 function ifChecked () {
-<<<<<<< HEAD
-  if (check == null){
-    alert("Check all the boxes");
-    location.reload()
-  }
-  else{
-
-}
-=======
   let input1 = document.getElementsByName('burger1');
   let input2 = document.getElementsByName('ice_cream1');
   let input3 = document.getElementsByName('tofu1');
@@ -25,7 +16,7 @@ function ifChecked () {
     }
     if (falseCount1 == input1.length){
       alert("You forgot a box!");
-    };
+    };}
   let falseCount2 = 0;
   for (b = 0; b < input2.length; b++){
     let check2 = input2[b].checked;
@@ -37,8 +28,41 @@ function ifChecked () {
     }
     if (falseCount2 == input2.length){
       alert("You forgot a box!");
+    };}
+    let falseCount3 = 0;
+    for (c = 0; c < input3.length; c++){
+      let check3 = input3[c].checked;
+      if (check3){
+        break
+      }
+      else{
+        falseCount3 += 1;
+      }
+      if (falseCount3 == input3.length){
+        alert("You forgot a box!");
+      }}
+  let falseCount4 = 0;
+  for (d = 0; d < input4.length; d++){
+    let check4 = input4[b].checked;
+    if (check4){
+      break
+    }
+    else{
+      falseCount4 += 1;
+    }
+    if (falseCount4 == input4.length){
+      alert("You forgot a box!");
+    }; };
+  let falseCount5 = 0;
+  for (e = 0; e < input5.length; e++){
+    let check5 = input5[e].checked;
+    if (check5){
+      break
+    }
+    else{
+      falseCount5 += 1;
+    }
+    if (falseCount5 == input5.length){
+      alert("You forgot a box!");
     };
-  }
-  };
-  }
->>>>>>> ef4a93d68a3dcce699b5da2a99d134ff24493e82
+  }; }
