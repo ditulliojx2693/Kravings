1dd7db49d5a4a705ec394dba316989ca12c7a585
Hannah <hannahsalameh18@gmail.com>
2019-07-31 15:28:20 -0700

    deleted sign up page

--- main.py
+++ main.py
@@ -61,57 +61,6 @@ class SignInPage(webapp2.RequestHandler):
         signin_template = the_jinja_env.get_template('templates/signin.html')
         self.response.write(signin_template.render())
 
-# class SignUpPage(webapp2.RequestHandler):
-#     def get(self):
-#         signup_template = the_jinja_env.get_template('templates/signup.html')
-#         self.response.write(signup_template.render())
-#     def get(self):
-#       user = users.get_current_user()
-#       # If the user is logged in...
-#       if user:
-#         email_address = user.nickname()
-#         cssi_user = CssiUser.get_by_id(user.user_id())
-#         signout_link_html = '<a href="%s">sign out</a>' % (
-#             users.create_logout_url('/'))
-#         # If the user has previously been to our site, we greet them!
-#         if cssi_user:
-#           self.response.write('''
-#               Welcome %s %s (%s)! <br> %s <br>''' % (
-#                 cssi_user.first_name,
-#                 cssi_user.last_name,
-#                 email_address,
-#                 signout_link_html))
-#         # If the user hasn't been to our site, we ask them to sign up
-#         else:
-#           self.response.write('''
-#               Welcome to our site, %s!  Please sign up! <br>
-#               <form method="post" action="/">
-#               <input type="text" name="first_name">
-#               <input type="text" name="last_name">
-#               <input type="submit">
-#               </form><br> %s <br>
-#               ''' % (email_address, signout_link_html))
-#       # Otherwise, the user isn't logged in!
-#       else:
-#         self.response.write('''
-#           Please log in to use our site! <br>
-#           <a href="%s">Sign in</a>''' % (
-#             users.create_login_url('/home')))
-    #
-    # def post(self):
-    #   user = users.get_current_user()
-    #   if not user:
-    #     # You shouldn't be able to get here without being logged in
-    #     self.error(500)
-    #     return
-    #   cssi_user = CssiUser(
-    #       first_name=self.request.get('first_name'),
-    #       last_name=self.request.get('last_name'),
-    #       id=user.user_id())
-    #   cssi_user.put()
-    #   self.response.write('Thanks for signing up, %s!' %
-    #       cssi_user.first_name)
-
 class QuizPage(webapp2.RequestHandler):
     def get(self):  # for a get request
         quiz_template = the_jinja_env.get_template('templates/quiz.html')
