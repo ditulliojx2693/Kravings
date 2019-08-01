3a4b398de7be6085be3072720711971f6393e06a
Hannah <hannahsalameh18@gmail.com>
2019-08-01 09:54:42 -0700

    took out unnecessary code that breaks the log in

--- main.py
+++ main.py
@@ -42,18 +42,10 @@ class HomePage(webapp2.RequestHandler):
         redirect_template = the_jinja_env.get_template('templates/redirect.html')
         username_attempt = self.request.get('usernameAttempt')
         password_attempt = self.request.get('passwordAttempt')
-        check_cred = UserData.query().filter(UserData.username == username_attempt, UserData.password == password_attempt).fetch(1)
+        check_cred = UserData.query().filter(UserData.username == username_attempt, UserData.password == password_attempt).fetch()
         if len(check_cred) == 0:
             self.response.write(redirect_template.render())
-            user = check_cred[0].key.get()
-            user.loggedin = False
-            user.put()
         else:
-            user = check_cred[0].key.get()
-            userInfo = check_cred[0]
-            print(self.request.cookies.get("loggedin"))
-            user.loggedin = True
-            user.put()
             self.response.set_cookie("loggedin", userInfo.username)
             self.response.set_cookie("firstname", userInfo.first_name)
             self.response.set_cookie("lastname", userInfo.last_name)
